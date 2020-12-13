6b5676ce2384c3820f6a745074be424bac264821
David Murphy <demurphy16@brandeis.edu>
2020-12-13 13:08:33 -0500

    desktop styling and functionality compelte

--- my-portfolio/src/screens/Projects.jsx
+++ my-portfolio/src/screens/Projects.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Projects.css'
 import cooked from '../assets/cooked.gif'
 import block from '../assets/block.gif'
+import beer from '../assets/beer.gif'
 
 function Projects() {
 
@@ -17,6 +18,9 @@ function Projects() {
           and then proceed to create a recipe with an image, cooktime, and ingredients. It is an unfinished application that is missing 
           some functionality and styling but works nonetheless. 
         </p>
+        <button className="project-button cooked-button"> Use Project
+          <a href="http://superb-memory.surge.sh/"></a>    
+        </button>
         </div>
       </div>
       <div className="project-container block">
@@ -26,17 +30,24 @@ function Projects() {
           Block was desgined by a team of UX students and uses Express for the backend with React on the frontend.
           I was responsible for building out the pages and pair programming the backend. 
         </p>
+        <button className="project-button"> Use Project
+          <a href="https://the-block-app.netlify.app/"></a>    
+        </button>
         </div>
       </div>
       <div className="project-container beer-storage">
-        <img className="project-img" />
+        <img className="project-img" src={beer}/>
         <div className="project-description-container">
         <p className="project-description">Beer-Storage was the second application I ever built. It is a fontend application designed to help Users
           find the right beer. Beer-Storage uses React to render its information and was built after just 2 weeks of instruction. 
         </p>
+        <button className="project-button"> Use Project
+          <a href="https://happy-bell-026f8e.netlify.app"></a>    
+        </button>
         </div>
       </div>
     </div>
   )
 }
 export default Projects
+
