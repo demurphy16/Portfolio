afd71b568656ed107f846e1c5da49123dbbb50c1
David Murphy <demurphy16@brandeis.edu>
2020-12-13 13:50:57 -0500

    mobile breapoint styled for home page

--- my-portfolio/src/layout/Footer.jsx
+++ my-portfolio/src/layout/Footer.jsx
@@ -5,14 +5,14 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer-container">
-        <p>978-877-1945</p>
+        <p className="footer-text">978-877-1945</p>
         <a href="https://github.com/demurphy16" target="_blank" className="icon-anchor">
           <img className="footer-icon" alt="icon" src="https://www.flaticon.com/svg/static/icons/svg/1322/1322104.svg"/>
         </a>
         <a href="https://www.linkedin.com/in/david-murphy-a46453181/" target="_blank"className="icon-anchor">
         <img className="footer-icon" alt="icon" src="https://www.flaticon.com/svg/static/icons/svg/725/725397.svg"/>
         </a>
-        <p>demuprhy16@brandeis.edu</p>
+        <p className="footer-text">demuprhy16@brandeis.edu</p>
       </div>
     </footer>
   )
